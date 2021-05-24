6f9c837a078b338e2da07033f58b0dea650aa712
gamingbuddhist <68414004+gamingbuddhist@users.noreply.github.com>
2021-05-24 15:13:44 -0400

    the main shtuff
    reupping everything

---
+++ modules/CEO/ceo.py
@@ -0,0 +1,64 @@
+import sqlite3
+import sys
+
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+# from modules.ACCT.time_clock.time import TimeSystem
+
+class ChiefExecOfficer(QMainWindow):
+
+    def __init__(self, window_manager):
+        super().__init__()
+
+        self.setWindowTitle("KasMek, LLC - CEO Station")
+        self.setStyleSheet("background-color : black")
+        self.UiComponents()
+        self.showMaximized()
+    
+    def UiComponents(self):
+
+        self.h_r = QPushButton("Human\nResources", self)
+        self.h_r.setGeometry(200, 100, 100, 50)
+        self.h_r.setStyleSheet("border : 1px solid green; color : green; text-decoration : underline")
+        self.h_r.clicked.connect(self.human_resources)
+
+        self.acct = QPushButton("Accounting", self)
+        self.acct.setGeometry(350, 100, 100, 50)
+        self.acct.setStyleSheet("border : 1px solid green; color : green; text-decoration : underline")
+        self.acct.clicked.connect(self.accounting)
+
+        self.timeclock = QPushButton("Time\nClock", self)
+        self.timeclock.setGeometry(200, 150, 100, 50)
+        self.timeclock.setStyleSheet("border : 1px solid green; color : green; text-decoration : underline")
+        self.timeclock.clicked.connect(self.time_clock)
+
+        self.s_d = QPushButton("Software Development\Department", self)
+        self.s_d.setGeometry(350, 150, 100, 50)
+        self.s_d.setStyleSheet("border : 1px solid green; color : green; text-decoration : underline")
+        self.s_d.clicked.connect(self.software_dept)
+
+        self.c_s = QPushButton("Cyber\nSecurity", self)
+        self.c_s.setGeometry(200, 200, 100, 50)
+        self.c_s.setStyleSheet("border : 1px solid green; color : green; text-decoration : underline")
+        self.c_s.clicked.connect(self.cyber_sec)
+
+    def human_resources(self):
+
+        pass
+
+    def accounting(self):
+
+        pass
+
+    def time_clock(self):
+
+        pass
+
+    def software_dept(self):
+
+        pass
+
+    def cyber_sec(self):
+
+        pass
